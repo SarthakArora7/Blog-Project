d4e0ad868cb9b59ed2b304c476799b3045343e79
Sarthak Arora <matar.batashe@gmail.com>
2025-08-15 14:16:30 +0530

    Initial Commit: React + Django Blog Project

---
+++ backend/api/models.py
@@ -0,0 +1,170 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.utils.html import mark_safe
+from django.utils.text import slugify
+from shortuuid.django_fields import ShortUUIDField
+import shortuuid
+
+class User(AbstractUser):
+    username = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(unique=True) 
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    otp = models.CharField(max_length=100, null=True, blank=True)
+    
+    USERNAME_FIELD = 'email'  # The USERNAME_FIELD attribute specifies which field should be used as the unique identifier for authentication.
+    REQUIRED_FIELDS = ['username']  # The REQUIRED_FIELDS attribute is a list of fields that are required when creating a user via the Django admin or through the create_user method.
+    # email and username must be provided by the user as they are required.
+
+    def __str__(self):
+        return self.email
+    
+    def save(self, *args, **kwargs):  # defining a custom save method.
+        email_username, mobile = self.email.split("@")
+        if self.full_name == "" or self.full_name == None:
+            self.full_name = email_username
+        if self.username == "" or self.username == None:
+            self.username = email_username  
+    
+        super(User, self).save(*args, **kwargs)  # calling the "real" save method.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="image", default="default/default-user.jpg", null=True, blank=True)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    author = models.BooleanField(default=False)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    facebook = models.CharField(max_length=100, null=True, blank=True)
+    twitter = models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.full_name:
+            return str(self.full_name)
+        else:
+            return str(self.user.full_name)
+    
+
+    def save(self, *args, **kwargs):
+        if self.full_name == "" or self.full_name == None:
+            self.full_name = self.user.full_name
+        super(Profile, self).save(*args, **kwargs)
+    
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+# post_save is a signal triggered after a model is saved.
+# Commonly used for actions like creating a profile after user creation.
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to="image", null=True, blank=True)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = "Category"
+
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+    
+    def post_count(self):
+        return Post.objects.filter(category=self).count()  # queries the Post model to retrieve all posts with category = current_instance and counts it.
+
+class Post(models.Model):
+    STATUS = ( 
+        ("Active", "Active"), 
+        ("Draft", "Draft"),
+        ("Disabled", "Disabled"),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to="image", null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    tags = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='posts')
+    status = models.CharField(max_length=100, choices=STATUS, default="Active")
+    view = models.IntegerField(default=0)
+    likes = models.ManyToManyField(User, blank=True, related_name="likes_user")  # In Django, the related_name attribute in a ManyToManyField (or any other relationship field) is used to specify the name of the reverse relation from the related model back to the model that defines the relationship. This allows you to access related objects in a more intuitive way.
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = "Post"
+
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title) + "-" + shortuuid.uuid()[:2]
+        super(Post, self).save(*args, **kwargs)
+    
+    def comments(self):
+        return Comment.objects.filter(post=self).order_by("-id")  
+    # Comment.objects.filter(post=self): This line queries the Comment model to retrieve all comment instances that are related to the specific post instance represented by self.
+    # The filter(post=self) part assumes that the Comment model has a foreign key field named post that links each comment to a specific post. This means that each comment is associated with a particular post.
+
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    comment = models.TextField()
+    reply = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.post.title} - {self.name}"
+    
+    class Meta:
+        verbose_name_plural = "Comment"
+
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.post.title} - {self.user.username}"
+    
+    class Meta:
+        verbose_name_plural = "Bookmark"
+
+
+class Notification(models.Model):
+    NOTI_TYPE = ( ("Like", "Like"), ("Comment", "Comment"), ("Bookmark", "Bookmark"))
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100, choices=NOTI_TYPE)
+    seen = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Notification"
+    
+    def __str__(self):
+        if self.post:
+            return f"{self.type} - {self.post.title}"
+        else:
+            return "Notification"
+        
+
+
