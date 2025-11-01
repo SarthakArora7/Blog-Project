c8597e7ea7d9439d0335678c55358a26f4aea361
Sarthak Arora <matar.batashe@gmail.com>
2025-11-01 14:29:49 +0530

    ImageUpload

--- backend/api/models.py
+++ backend/api/models.py
@@ -95,7 +95,7 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=100)
-    image = models.FileField(upload_to="image", null=True, blank=True)
+    image = models.ImageField(upload_to="image", null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     tags = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='posts')
